19463abaf32e454b1e6927b07fc08cd2a09c3e1c
devil-mate <devil-mate@outlook.com>
2022-05-11 15:01:21 +0800

    pcl读取并显示,单个pcd

--- pra_/src/PclOps.cpp
+++ pra_/src/PclOps.cpp
@@ -1,8 +1,25 @@
 
 #include <stdio.h>
 #include "PclOps.h"
+void viewerOneOff(pcl::visualization::PCLVisualizer& viewer)
+{
+	//设置背景颜色
+	viewer.setBackgroundColor(1.0, 0, 1.0);
+}
 
 void print(){
-    printf("this is a pclOpas\n");
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud1(new     pcl::PointCloud<pcl::PointXYZRGBA>);
+    printf("this is a pclOpas---\n");
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1(new  pcl::PointCloud<pcl::PointXYZ>);
+    int8_t res=pcl::io::loadPCDFile("/home/zj/share_sdb5/tempData/DataSave/testLoam_pcd/1422133392.537796096.pcd", *cloud1); //加载点云文件  
+    printf("load pcd res: \n",res);
+    if(res==-1){
+        printf("load pcd error\n");
+    }
+	pcl::visualization::CloudViewer viewer("Cloud Viewer");//创建viewer对象  
+	// //blocks until the cloud is actually rendered  
+	viewer.showCloud(cloud1);//将pcb与viewer对象联系起来
+	// viewer.runOnVisualizationThreadOnce(viewerOneOff);
+	while (!viewer.wasStopped())//要想让自己所创窗口一直显示，则加上 while (!viewer.wasStopped()){ };即可.
+	{
+	}
 }
