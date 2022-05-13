86b976112fd3028af51cad849efed2c9f6bf7dd0
devil-mate <devil-mate@outlook.com>
2022-05-13 19:01:11 +0800

    x

---
+++ pra_/src/PCL/PclLoad.h
@@ -0,0 +1,10 @@
+#ifndef _PCL_LOAD_H
+#define _PCL_LOAD_H
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/point_cloud.h>  
+#include <pcl/visualization/cloud_viewer.h>  
+
+
+#endif
